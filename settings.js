d5c0cdcef72a8d1f472622342435216a0c0f1f26
KokoRey <106365965+AkuBott@users.noreply.github.com>
2022-05-28 23:06:25 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,16 +21,16 @@ global.auto_welcome_msg = true
 global.auto_leave_msg = true
 global.prefix = "."
 global.language = "ind"
-global.ownerNumberr = "62887435047326"
-global.ownerbot = ['62887435047326@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.botname = "Alphabot"
+global.ownerNumberr = "6285232604817"
+global.ownerbot = ['6285232604817@s.whatsapp.net','6285771052661@s.whatsapp.net'] // .owner
+global.ownername = "ᴹᴿ᭄ ®KOKO•REY✓ ×፝֟͜×"
+global.botname = "KoKoReyBot"
 global.thumb = "thumbnail_1.jpg"
 global.thumbnail ="thumbnail_2_1.jpg"
 global.fakethumb ="thumbnail_1.jpg"
 global.donasi ="donasi.jpg"
 global.captionpay ="Scan untuk melakukan pembayaran"
-global.fake = "「  ALPHABOT  」\nCreated By ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.fake = "「  KOKO  」\nCreated By ᴹᴿ᭄ ®.REY✓ ×፝֟͜×"
 global.alphaVersion = [2, 2143, 3] //WhatsApp web version
 
 /* Available type menu
